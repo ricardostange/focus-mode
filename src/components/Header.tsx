f402197a79d5b874d865c401bdef2e3a2f8950df
Ricardo Vizzotto <ricstange@gmail.com>
2026-04-27 17:19:31 -0300

    favicon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -18,12 +18,8 @@ export function Header({ showSearch, searchTerm, onSearchChange, onLogoClick, on
   return (
     <header className="top-bar">
       <div className="header-content">
-        <a href="/" onClick={handleLogoClick}>
-          <img 
-            src="https://upload.wikimedia.org/wikipedia/commons/b/b8/YouTube_Logo_2017.svg" 
-            id="home-logo" 
-            alt="Logo" 
-          />
+        <a href="/" onClick={handleLogoClick} className="logo-text">
+          Focus Mode
         </a>
         <div className="search-container" style={{ visibility: showSearch ? 'visible' : 'hidden' }}>
           <input 
